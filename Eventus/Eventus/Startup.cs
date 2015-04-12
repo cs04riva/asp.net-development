f41a6c6fa9dc39fcf9fee98a555407e4e9e514c6
cs04riva <chrisrivadeneira@outlook.com>
2015-04-11 20:41:10 -0400

    Default Project Files Created

---
+++ Eventus/Eventus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eventus.Startup))]
+namespace Eventus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
