77364910dc2b16b76fa519a9b5dd2593bca8932b
cs04riva <chrisrivadeneira@outlook.com>
2015-04-29 09:15:17 -0400

    Setting up ViewModel for Profile Page

---
+++ EventApplication/EventApplication/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventApplication.ViewModels
+{
+    public class ProfileViewModel
+    {
+        //Get User Name
+
+        //Get Events
+
+
+    }
+}
